b735bcb064957a914b6019839ae32678a44ab7e5
4414 Писарева Д.О. <dianapisareva060405@gmail.com>
2025-11-15 19:36:41 +0300

    Revert "Добавлен второй комментарий"
    This reverts commit 6a7a1e53a13499798d0c190501f43833bbaaa1ed.

--- main.cpp
+++ main.cpp
@@ -10,7 +10,6 @@
 using namespace std;
 
 // комментарий
-// второй комментарий
 // Шаблонная функция для заполнения контейнера случайными числами
 template<typename Container>
 void fillContainer(Container& cont, size_t count) {
